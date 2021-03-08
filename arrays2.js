17b6224e0f9b961a0996991273f0e9c914c29438
Dana <dana.lalovaod@gmail.com>
2021-03-08 22:03:43 +0200

    feat: add square digits task

--- arrays2.js
+++ arrays2.js
@@ -20,4 +20,27 @@
 
 //=====================
 
-console.log("2 task array - Square Every Digit"); 
+console.log("2 task array - Square Every Digit");
+
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+
+
+function squareDigits(num) {
+    let stringDigits = String(num).split("");
+    // console.log(stringDigits);
+    
+    return console.log(Number(stringDigits.map(numb => {
+        numb = Number(numb);
+        let square = numb * numb;
+        let total = "";
+        total+= square;
+        return total;
+    }).reduce((acc, value) => acc + value)));
+}
+
+squareDigits(9119); // 811181
